2ee5ba278ef3f65d2019dc54f56535356ca61cdc
AmitxParmar <amitparmar901@gmail.com>
2023-01-16 00:02:58 +0530

    add dateBuilder and Search functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,36 @@ import { useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState<String>('');
+  const [weather, setWeather] = useState<Object>({});
 
   const api = {
     key: process.env.NEXT_APP_WEATHER_API_KEY,
     base: "https://api.openweathermap.org/data/2.5/"
   }
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
 
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <>
       <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
