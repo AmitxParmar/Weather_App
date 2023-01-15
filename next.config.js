ecfc2ef9a0e030a3422f4fd761af0e64235122b1
AmitxParmar <amitparmar901@gmail.com>
2023-01-15 23:56:26 +0530

    setup api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_APP_WEATHER_API_KEY: '5b5d697b5b7741c79d4e9ba980fe9539',
+  },
 }
 
 module.exports = nextConfig
